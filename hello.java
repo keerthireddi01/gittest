c8f370a3e0d8e579f4c2ff3a613a839c9b83b7ae
keerthireddi01 <45559187+keerthireddi01@users.noreply.github.com>
2018-12-10 11:47:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@ public class hello{
         System.out.println("Hello, jenkins");
         
         System.out.println("Hello, jenkins");
-        System.out.println("SDGDFYHERTEW");
+      
         System.out.println("SJKDFHKSDJGH");
     }
 
